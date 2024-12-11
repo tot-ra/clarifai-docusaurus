147b0ea1282a150fe70e121bf37bb61006b505ae
Artjom Kurapov <artkurapov@gmail.com>
2024-12-11 23:05:38 +0200

    reduce payload passed to frontend

--- SearchBar.js
+++ SearchBar.js
@@ -15,55 +15,57 @@ function debounce(func, wait) {
 
 export default function SearchBarWrapper(props) {
 
-  let [searchResults, setResults] = useState([])
+    let [searchResults, setResults] = useState([])
 
-  function search(e) {
-    if (e.target.value.length <3) return
-  
-    fetch('http://localhost:5000/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ 
-        text: e.target.value,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Success:', data);
+    function search(e) {
+        if (e.target.value.length <3) return
 
-        setResults(data)
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
-  
+        fetch('http://localhost:5000/search', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                text: e.target.value,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
 
-  return (
-    <>
-      <input type="text" 
-      placeholder="Search" 
-      onChange={debounce(search, 300)}
-      onBlur={search} 
-      onSubmit={search}
-      style={{padding:"7px 15px", border: "2px solid #CCC", borderRadius: 5}} />
-
-    {searchResults && searchResults.hits && searchResults.hits.length > 0 &&
-        <div style={{
-            background: "white",
-            color: "black",
-            padding: 20,
-            position: "absolute"
-        }}>
-            {searchResults.hits.map((hit, index) => {
-            return <div key={index} style={{padding:3}}><a
-            style={{color: "blue", padding: 3}}
-            href={"/" + hit.input.data.metadata.url}>{hit.input.data.metadata.title}</a></div>
-            })}
-        </div>
+                setResults(data)
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
-    </>
-  );
+
+
+    return (
+        <>
+            <input type="text"
+                   placeholder="Search"
+                   onChange={debounce(search, 300)}
+                   onBlur={search}
+                   onSubmit={search}
+                   style={{padding:"7px 15px", border: "2px solid #CCC", borderRadius: 5}} />
+
+            {searchResults && searchResults.length > 0 &&
+                <div style={{
+                    background: "white",
+                    color: "black",
+                    padding: 20,
+                    position: "absolute",
+                    border: "1px solid #CCC",
+                }}>
+                    {searchResults.map((hit, index) => {
+                        return <div key={index} style={{padding:3}}><a
+                            style={{color: "blue", padding: 3}}
+                            href={"/" + hit.url}>{hit.title}</a></div>
+
+                    })}
+                </div>
+            }
+        </>
+    );
 }
--- proxy-search.js
+++ proxy-search.js
@@ -1,81 +1,91 @@
 require('dotenv').config();
-const fastify = require('fastify')({ logger: true });
+const fastify = require('fastify')({logger: true});
 const PORT = 5000;
 
 // add CORS SUPPORT
 fastify.register(require('@fastify/cors'), {
-  origin: '*',
+    // add your own domain for security
+    origin: '*',
 });
 
 
 // Fastify route for POST /search
 fastify.post('/search', async (request, reply) => {
-  try {
-    const raw = JSON.stringify({
-      "user_app_id": {
-          "user_id": process.env.CLARIFAI_USER_ID,
-          "app_id": process.env.CLARIFAI_APP_ID
-      },
-      "pagination": {
-          "per_page": 10
-      },
-      "searches": [
-          {
-              "query": {
-                  "ranks": [
-                      {
-                          "annotation": {
-                              "data": {
-                                  "text": {
-                                      "raw": request.body.text
-                                  }
-                              }
-                          }
-                      }
-                  ]
-              }
-          }
-      ]
-  });
+    try {
+        const raw = JSON.stringify({
+            "user_app_id": {
+                "user_id": process.env.CLARIFAI_USER_ID,
+                "app_id": process.env.CLARIFAI_APP_ID
+            },
+            "pagination": {
+                "per_page": 10
+            },
+            "searches": [
+                {
+                    "query": {
+                        "ranks": [
+                            {
+                                "annotation": {
+                                    "data": {
+                                        "text": {
+                                            "raw": request.body.text
+                                        }
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        });
 
-  const requestOptions = {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Key ' + process.env.CLARIFAI_PAT
-      },
-      body: raw
-  };
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Key ' + process.env.CLARIFAI_PAT
+            },
+            body: raw
+        };
 
-  out = await fetch(`https://api.clarifai.com/v2/inputs/searches?per_page=10`, requestOptions)
+        out = await fetch(`https://api.clarifai.com/v2/inputs/searches?per_page=10`, requestOptions)
 
 
-  //convert readable stream to string
-  out = await out.json();
+        //convert readable stream to string
+        out = await out.json();
 
-  console.dir(out, {depth: null});
+        console.dir(out, {depth: null});
 
-  reply.send(out);
-    
-  } catch (error) {
-    // Handle errors
-    fastify.log.error(error);
-    reply.status(500).send({
-      error: 'Failed to fetch results from Clarifai',
-      details: error.message,
-    });
-  }
+        // cleanup results
+        results = out.hits.map(hit => {
+            return {
+                score: hit.score,
+                title: hit.input.data.metadata.title,
+                url: hit.input.data.metadata.url,
+            };
+        });
+
+        reply.send(results);
+
+    } catch (error) {
+        // Handle errors
+        fastify.log.error(error);
+        reply.status(500).send({
+            error: 'Failed to fetch results from Clarifai',
+            details: error.message,
+        });
+    }
 });
 
 // Start the Fastify server
 const start = async () => {
-  try {
-    await fastify.listen({ port: PORT });
-    fastify.log.info(`Server is running on http://localhost:${PORT}`);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+    try {
+        await fastify.listen({port: PORT});
+        fastify.log.info(`Server is running on http://localhost:${PORT}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
 };
 
 start();
